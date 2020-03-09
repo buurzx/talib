487db79e419c59796b94622b3f0d0ad528900bc0
Pavel Ro <buurzx@gmail.com>
2020-03-09 21:25:29 +0300

    Remove macd from indicatior and formatting

--- lib/talib/sma.ex
+++ lib/talib/sma.ex
@@ -4,8 +4,8 @@ defmodule Talib.SMA do
 
   ## History
 
-  Version: 1.0  
-  Source: https://qkdb.wordpress.com/2013/04/22/simple-moving-average/  
+  Version: 1.0
+  Source: https://qkdb.wordpress.com/2013/04/22/simple-moving-average/
   Audited by:
 
   | Name         | Title             |
@@ -21,10 +21,8 @@ defmodule Talib.SMA do
   * :values - List of values resulting from the calculation
   """
   @type t :: %Talib.SMA{period: integer, values: [number]}
-  defstruct [
-    period: 0,
-    values: []
-  ]
+  defstruct period: 0,
+            values: []
 
   @doc """
   Gets the SMA of a list.
@@ -45,7 +43,7 @@ defmodule Talib.SMA do
       iex> Talib.SMA.from_list([17], 0)
       {:error, :bad_period}
   """
-  @spec from_list([number], integer) :: {:ok, Talib.SMA.t} | {:error, atom}
+  @spec from_list([number], integer) :: {:ok, Talib.SMA.t()} | {:error, atom}
   def from_list(data, period), do: calculate(data, period)
 
   @doc """
@@ -68,38 +66,47 @@ defmodule Talib.SMA do
       iex> Talib.SMA.from_list!([17], 0)
       ** (BadPeriodError) bad period error
   """
-  @spec from_list!([number], integer) :: Talib.SMA.t | no_return
+  @spec from_list!([number], integer) :: Talib.SMA.t() | no_return
   def from_list!(data, period) do
     case calculate(data, period) do
       {:ok, result} -> result
-      {:error, :no_data} -> raise NoDataError 
-      {:error, :bad_period} -> raise BadPeriodError 
+      {:error, :no_data} -> raise NoDataError
+      {:error, :bad_period} -> raise BadPeriodError
     end
   end
 
   @doc false
-  @spec calculate([number], integer, [float]) :: {:ok, Talib.SMA.t}
-  | {:error, atom}
+  @spec calculate([number], integer, [float]) ::
+          {:ok, Talib.SMA.t()}
+          | {:error, atom}
   defp calculate(data, period, results \\ [])
+
   defp calculate([], _period, []),
     do: {:error, :no_data}
+
   defp calculate(_data, 0, _results),
     do: {:error, :bad_period}
+
   defp calculate([], period, results),
     do: {:ok, %Talib.SMA{period: period, values: results}}
+
   defp calculate([hd | tl] = data, period, results) do
     cond do
-      length(results) < (period - 1) && length(data) > length(results) ->
+      length(results) < period - 1 && length(data) > length(results) ->
         calculate(data, period, results ++ [nil])
+
       length(data) < period ->
         calculate(tl, period, results)
+
       hd === nil ->
         calculate(tl, period, results ++ [nil])
+
       length(data) >= period ->
-        result = data
-        |> Enum.take(period)
-        |> Enum.sum
-        |> Kernel./(period)
+        result =
+          data
+          |> Enum.take(period)
+          |> Enum.sum()
+          |> Kernel./(period)
 
         calculate(tl, period, results ++ [result])
     end
